ae75d1444756bfe6f0e39f086d53652edd5070ce
razor <razorcd@yahoo.com>
2015-11-27 21:34:08 +0200

    gulp: build index and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var inject = require('gulp-inject');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+// var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
 
-gulp.task('sass', function () {
-  return sass('app/stylesheets/**/*.scss')
-    .on('error', sass.logError)
-    .pipe(gulp.dest('dist/stylesheets'));
+gulp.task('build_css', function () {
+  var sass = require('gulp-sass');
+  var minifyCss = require('gulp-minify-css');
+
+  return gulp.src('app/stylesheets/**/*.scss')
+             // .pipe(sourcemaps.init())
+               .pipe(sass())
+               .pipe(concat('main.css'))
+               .pipe(minifyCss())
+             // .pipe(sourcemaps.write())
+             .pipe(gulp.dest('dist/stylesheets'));
+});
+
+
+
+gulp.task('build_index', function () {
+  var sources = gulp.src(['dist/**/*.js', 'dist/**/*.css'], {read: false});
+
+  return gulp.src('app/index.html')
+             .pipe(gulp.dest('dist'))
+             .pipe(inject(sources, {relative: true}))
+             .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+             .pipe(gulp.dest('dist'));
 });
